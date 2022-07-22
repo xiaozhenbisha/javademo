2ec4f50f256c45691385589fe42a52ed514e04a6
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-22 19:49:01 +0800

    Create RelationalOp.java

---
+++ javadoc/javajiangyi/src/theme4modifydata/RelationalOp.java
@@ -0,0 +1,10 @@
+package theme4modifydata;
+
+public class RelationalOp {
+    public static void main(String args[]){
+        float a =10.0f;
+        double b = 10.0;
+        boolean	result	=	a==b;
+        System.out.println(result);
+    }
+}
