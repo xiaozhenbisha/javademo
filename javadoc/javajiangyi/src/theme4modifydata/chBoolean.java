4485e8587785cbec98e486f68fa90bab34f5adc9
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-23 00:27:23 +0800

    Create chBoolean.java

---
+++ javadoc/javajiangyi/src/theme4modifydata/chBoolean.java
@@ -0,0 +1,15 @@
+package theme4modifydata;
+
+/**
+ * @author shkstart
+ * @create 2022-07-22 23:13
+ */
+
+public class chBoolean {
+    public static void main(String[] args) {
+        int	x=3,y=4	;
+        boolean	ch	;
+        ch=x<y||((++x)==	(--y));
+        System.out.println(ch);
+    }
+}
