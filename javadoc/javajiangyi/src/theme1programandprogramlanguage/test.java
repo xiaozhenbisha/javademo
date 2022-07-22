dc04c15160322283276fdc0f66a9fcce1d33aaf8
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-22 19:45:41 +0800

    Create test.java

---
+++ javadoc/javajiangyi/src/theme1programandprogramlanguage/test.java
@@ -0,0 +1,15 @@
+package theme1programandprogramlanguage;
+
+public class test {
+    public static void main(String[] args){
+        int	i=10,j=3;
+        System.out.println(i/j);
+        System.out.println(i+j);
+        System.out.println(i-j);
+    }
+}
+/*
+程序执行 10/3 后，输出：3
+程序执行 10+3 后，输出：13
+程序执行 10-3 后，输出：7
+ */
