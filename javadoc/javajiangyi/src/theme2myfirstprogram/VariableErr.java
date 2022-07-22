37ab03974e3969f2dbf2e528f2217ac3c3a2827a
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-22 19:49:12 +0800

    Create VariableErr.java

---
+++ javadoc/javajiangyi/src/theme2myfirstprogram/VariableErr.java
@@ -0,0 +1,11 @@
+package theme2myfirstprogram;
+
+public class VariableErr {
+    public static void main(String[]arguments){
+        int ABC;
+        float MyWord;
+        char f2;
+        String St4;
+
+    }
+}
