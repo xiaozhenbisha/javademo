5da9703f64afea3261c1be6cde2c882428d9ce8a
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-22 19:48:50 +0800

    Create EasyJava.java

---
+++ javadoc/javajiangyi/src/theme2myfirstprogram/EasyJava.java
@@ -0,0 +1,15 @@
+package theme2myfirstprogram;
+
+public class EasyJava {
+    public static void main(String[] args){
+       // System.out.println("Hello world!");
+        int egg = 5;
+        System.out.println("egg: "+egg);
+        float gradeAverage = 84.5f; System.out.println("gradeAverage: "+ gradeAverage);
+        char key = 'F';
+        System.out.println("key: "+key);
+        String productName = "爱德牌电饭锅"; System.out.println("productName: "+ productName);
+        boolean gameOver = false;
+        System.out.println("gameOver: "+gameOver);
+    }
+}
