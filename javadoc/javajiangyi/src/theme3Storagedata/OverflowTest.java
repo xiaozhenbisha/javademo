7dd6f14e9e815b1c00ea2223aaeb1b8c6b5959ba
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-22 19:49:09 +0800

    Create OverflowTest.java

---
+++ javadoc/javajiangyi/src/theme3Storagedata/OverflowTest.java
@@ -0,0 +1,13 @@
+package theme3Storagedata;
+
+public class OverflowTest {
+    public static void main(String[] args) {
+        int big = 12345;
+        byte small = (byte)big;
+        System.out.println(small);
+    }
+}
+/**
+ *
+ *
+ */
