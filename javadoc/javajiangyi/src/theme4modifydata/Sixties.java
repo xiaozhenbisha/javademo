6a3fcb6a82e042e627094b86d280802130734a0c
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-23 00:27:21 +0800

    Create Sixties.java

---
+++ javadoc/javajiangyi/src/theme4modifydata/Sixties.java
@@ -0,0 +1,20 @@
+package theme4modifydata;
+
+/**
+ * @author shkstart
+ * @create 2022-07-22 23:15
+ */
+
+public class Sixties {
+    public	static	void	main(String[]	args)	{
+
+	 int	x	=	5;
+
+	 int	y	=	7;
+
+	 System.out.print(((y*2)%x));
+
+	 System.out.print("	"+(y%x));
+
+	 }
+}
