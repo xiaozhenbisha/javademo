9f713f3e6b70f9a9ad4f5219ac96ec0c952edba8
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-23 23:16:59 +0800

    Create Sanyuan.java

---
+++ javadoc/javajiangyi/src/theme5certainConditions/Sanyuan.java
@@ -0,0 +1,15 @@
+package theme5certainConditions;
+
+/**
+ * @author shkstart
+ * @create 2022-07-23 7:38
+ */
+
+public class Sanyuan {
+    public static void main(String[] args) {
+        int life = (int) Math.random();
+        System.out.println(life);
+        System.out.println((life == 0)? "Game is over" : "Continue");
+    }
+
+}
