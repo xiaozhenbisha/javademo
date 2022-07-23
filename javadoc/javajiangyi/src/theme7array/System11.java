1aebe0d6a33ad90d2001069f4d61b23212c955b7
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-23 23:17:46 +0800

    Create System11.java

---
+++ javadoc/javajiangyi/src/theme7array/System11.java
@@ -0,0 +1,15 @@
+package theme7array;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author shkstart
+ * @create 2022-07-23 22:57
+ */
+
+public class System11 {
+
+    public static void main(String[] args) {
+        System.out.println("sss");
+    }
+}
