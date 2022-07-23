906222f46f6e74dfebc263fcb0ed469d92afd35c
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-23 23:17:59 +0800

    Create User.java

---
+++ javadoc/javajiangyi/src/theme8objectword/User.java
@@ -0,0 +1,18 @@
+package theme8objectword;
+
+/**
+ * @author shkstart
+ * @create 2022-07-23 23:13
+ */
+
+public class User {
+    /**
+     * 成员变量的使用
+     * 类中变量使用格式是:类名.类变量名。例如:
+     */
+    static boolean isUser;
+    public static void main(String[] args){
+        User.isUser=true;
+        System.out.println(User.isUser);
+    }
+}
