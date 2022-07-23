ebc633b6ca3dd6de2feba3204e53064a1af90555
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-23 23:17:35 +0800

    Create WhileTets.java

---
+++ javadoc/javajiangyi/src/theme6loop/WhileTets.java
@@ -0,0 +1,17 @@
+package theme6loop;
+
+/**
+ * @author shkstart
+ * @create 2022-07-23 7:48
+ */
+
+public class WhileTets {
+    public static void main(String[] args) {
+        int limit=5;
+        int count=1;
+        while(count<limit){
+            System.out.println("*********");
+            count++;
+        }
+    }
+}
