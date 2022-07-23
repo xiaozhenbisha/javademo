461a879be613a47d95f3acc8eee688f4dbfce43a
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-23 23:18:08 +0800

    Create WhileLoopForever.java

---
+++ javadoc/javajiangyi/src/theme6loop/WhileLoopForever.java
@@ -0,0 +1,18 @@
+package theme6loop;
+
+/**
+ * @author shkstart
+ * @create 2022-07-23 7:49
+ */
+
+public class WhileLoopForever {
+    public static void main(String[] args) {
+
+        int count = 0;
+        while(true){
+            //if (count>100) break;
+            System.out.println("*********");
+            count++;
+        }
+    }
+}
