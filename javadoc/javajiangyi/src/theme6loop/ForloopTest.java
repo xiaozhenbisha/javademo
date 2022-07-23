7bec80e6fbeef32b594c6574d2b1bc85ffbbc32f
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-23 23:17:05 +0800

    Create ForloopTest.java

---
+++ javadoc/javajiangyi/src/theme6loop/ForloopTest.java
@@ -0,0 +1,15 @@
+package theme6loop;
+
+/**
+ * @author shkstart
+ * @create 2022-07-23 7:43
+ */
+
+public class ForloopTest {
+    public static void main(String[] args) {
+        for (int i=1; i<20; i++ ){ if (i%3 == 0) {
+            System.out.println("#: "+i);
+        }
+        }
+    }
+}
