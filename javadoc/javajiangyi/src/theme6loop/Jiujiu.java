93c34c62a7a9a3199092fa044973d61abdcb7cfc
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-23 23:17:08 +0800

    Create Jiujiu.java

---
+++ javadoc/javajiangyi/src/theme6loop/Jiujiu.java
@@ -0,0 +1,17 @@
+package theme6loop;
+
+/**
+ * @author shkstart
+ * @create 2022-07-23 19:39
+ */
+
+public class Jiujiu {
+    public static void main(String[] args){
+        int i,j;
+        for(i=1;i<=9;i++){
+            for(j=1;j<=9;j++){
+                System.out.println(i+"*"+j+" = "+i*j);
+            }
+        }
+    }
+}
