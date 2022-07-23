d23bf3537214f5870b7fd38eb21233438796666f
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-23 23:17:22 +0800

    Create Triple.java

---
+++ javadoc/javajiangyi/src/theme6loop/Triple.java
@@ -0,0 +1,15 @@
+package theme6loop;
+
+/**
+ * @author shkstart
+ * @create 2022-07-23 7:45
+ */
+
+public class Triple {
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++){
+            int result = i * 3;
+            System.out.print(result+" ");
+        }
+    }
+}
