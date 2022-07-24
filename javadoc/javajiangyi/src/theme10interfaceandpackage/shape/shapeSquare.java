1eae2d1219fd5a5230a8ea7e256e9e8b40edabc7
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-24 16:49:25 +0800

    Create shapeSquare.java

---
+++ javadoc/javajiangyi/src/theme10interfaceandpackage/shape/shapeSquare.java
@@ -0,0 +1,11 @@
+package theme10interfaceandpackage.shape;
+
+/**
+ * @author wzn
+ * @create 2022-07-24 16:43
+ */
+
+public interface shapeSquare {
+    abstract double getArea();
+    abstract double getPerimeter();
+}
