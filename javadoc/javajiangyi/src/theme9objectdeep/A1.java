6e5bd962824198a3405281dc638d98c56bf77e72
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-24 15:37:28 +0800

    Create A1.java

---
+++ javadoc/javajiangyi/src/theme9objectdeep/A1.java
@@ -0,0 +1,32 @@
+package theme9objectdeep;
+
+/**
+ * @author shkstart
+ * @create 2022-07-24 13:53
+ */
+
+public class A1 {
+    int	x=4;
+    int	y=1;
+    public	void printme()
+    {
+        System.out.println("x="+x+"	y="+y);
+    }
+}
+class B1 extends A1{
+    int	x;
+    public void printme()
+    {
+        int	z=super.x+6;
+        super.printme();
+        x=5;
+        System.out.println("z="+z+"x="+x);
+    }
+    public	static	void main(String arg[])
+    {
+        A1 a = new A1();
+        B1 b = new	B1();
+        a.printme();
+        b.printme();
+    }
+}
