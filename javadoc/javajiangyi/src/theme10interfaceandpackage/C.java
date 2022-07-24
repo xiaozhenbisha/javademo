bd2bdf1727219fcf1376944d6cc830bddc448c19
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-24 15:36:33 +0800

    Create C.java

---
+++ javadoc/javajiangyi/src/theme10interfaceandpackage/C.java
@@ -0,0 +1,15 @@
+package theme10interfaceandpackage;
+
+/**
+ * @author wzn
+ * @create 2022-07-24 15:33
+ */
+
+public class C implements A,B {
+    public int getAA(){
+        return a;
+    }
+    public float getBB(){
+        return b;
+    }
+}
