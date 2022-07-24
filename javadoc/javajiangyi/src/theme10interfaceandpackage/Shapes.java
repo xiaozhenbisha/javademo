4a6cdcab24cea285298e2277f16bd09efd34cdb0
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-24 15:36:51 +0800

    Create Shapes.java

---
+++ javadoc/javajiangyi/src/theme10interfaceandpackage/Shapes.java
@@ -0,0 +1,12 @@
+package theme10interfaceandpackage;
+
+/**
+ * @author wzn
+ * @create 2022-07-24 15:20
+ */
+
+public interface Shapes {
+    public final static double PI=3.14159;
+    abstract double getArea();
+    abstract double getPerimeter();
+}
