957f2f45bfa3da4ed59fc0a6a8fd6ada209c4af0
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-24 16:49:27 +0800

    Create shapeCircle.java

---
+++ javadoc/javajiangyi/src/theme10interfaceandpackage/shape/shapeCircle.java
@@ -0,0 +1,12 @@
+package theme10interfaceandpackage.shape;
+
+/**
+ * @author wzn
+ * @create 2022-07-24 16:43
+ */
+
+public class shapeCircle {
+    public static void main(String[] args){
+        System.out.println("Test package!");
+    }
+}
