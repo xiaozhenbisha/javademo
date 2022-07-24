39f4ecd29ec61c3989558e6e1e3c1ae8788a3938
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-24 15:36:29 +0800

    Create B.java

---
+++ javadoc/javajiangyi/src/theme10interfaceandpackage/B.java
@@ -0,0 +1,11 @@
+package theme10interfaceandpackage;
+
+/**
+ * @author wzn
+ * @create 2022-07-24 15:32
+ */
+
+public interface B {
+    public final static float b=4.5f;
+    abstract float getBB();
+}
