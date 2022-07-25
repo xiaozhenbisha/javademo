f5cc6d565f2c427dbb6e694c22e7aed90f8fbd53
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-25 11:26:17 +0800

    Create ConstructorTest2.java

---
+++ javadoc/javacommonlyusedclass/oop/src/com/java/constructor/ConstructorTest2.java
@@ -0,0 +1,62 @@
+package com.java.constructor;
+
+/**
+ * this 关键字：使用在本类中，代表当前对象，可以调用 属性、方法、构造器
+ *
+ * this.属性
+ * this.方法
+ * this(...) : 调用本类的构造器
+ * 	注意：
+ * 		①避免递归构造器调用（当使用this调用本类其他构造器时，至少有一个构造器中不使用 this）
+ * 		②this()调用本类构造器，必须使用在当前构造器中可执行代码的首行
+ */
+public class ConstructorTest2 {
+
+    public static void main(String[] args) {
+
+        Person1 p = new Person1("张三", 18, '男', 180, 55.55);
+
+        System.out.println(p.say());
+
+    }
+}
+
+class Person1{
+
+    private String name;
+    private int age;
+    private char gender;
+    private double height;
+    private double weight;
+
+    public Person1(){}
+
+    public Person1(String name){
+        this.name = name;
+    }
+
+    public Person1(String name, int age){
+        //
+        this(name);
+        this.age = age;
+    }
+
+    public Person1(String name, int age, char gender){
+        this(name, age);
+        this.gender = gender;
+    }
+
+    public Person1(String name, int age, char gender, double height){
+        this(name, age, gender);
+        this.height = height;
+    }
+
+    public Person1(String name, int age, char gender, double height, double weight){
+        this(name, age, gender, height);
+        this.weight = weight;
+    }
+
+    public String say(){
+        return name + "," + age + "," + gender + "," + height + "," + weight;
+    }
+}
