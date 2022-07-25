7ae986aae4ea5ee41c2fda2e977c11443f1f1b67
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-25 18:33:02 +0800

    Create ArrayTest8.java

---
+++ javadoc/javacommonlyusedclass/array/src/com/java/testarray/ArrayTest8.java
@@ -0,0 +1,70 @@
+package com.java.testarray;
+
+import java.util.Arrays;
+
+public class ArrayTest8 {
+    public static void main(String[] args) {
+        int[] array = {2, 3, 20, 0, -100};
+
+        //最大值
+        int max = array[0];
+        for(int i = 1; i < array.length; i++){
+            if(array[i] > max){
+                max = array[i];
+            }
+        }
+
+        System.out.println("最大值为：" + max);
+
+        //最小值
+        int min = array[0];
+        for(int i = 1; i < array.length; i++){
+            if(array[i] < min){
+                min = array[i];
+            }
+        }
+
+        System.out.println("最小值为：" + min);
+
+        //总和
+        int sum = 0; //计数器
+        for(int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+
+        System.out.println("总和为：" + sum);
+
+        //平均值
+        System.out.println("平均值为：" + (double)sum / array.length);
+
+        //数组的复制
+        int[] array2 = new int[array.length];
+
+        for(int i = 0; i < array2.length; i++){
+            array2[i] = array[i];
+        }
+
+        //数组的反转
+        for(int a : array){
+            System.out.print(a + "\t");
+        }
+
+        for(int i = 0; i < array.length / 2; i++){
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+
+
+        //数组的排序
+        Arrays.sort(array);
+
+
+        //数组的遍历
+        System.out.println();
+        for(int a : array){
+            System.out.print(a + "\t");
+        }
+
+    }
+}
