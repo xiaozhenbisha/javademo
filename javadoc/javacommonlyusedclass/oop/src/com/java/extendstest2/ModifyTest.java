eba84dbe85e30f570ac10e2408a2f6f9126e8aa1
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-26 10:31:44 +0800

    Create ModifyTest.java

---
+++ javadoc/javacommonlyusedclass/oop/src/com/java/extendstest2/ModifyTest.java
@@ -0,0 +1,25 @@
+package com.java.extendstest2;
+
+import com.java.aaa.Employee;
+
+/**
+ * 访问控制修饰符：
+ *
+ * private : 私有的，可用于修饰属性、方法。 只能在本类中访问
+ *
+ * default : 默认的(缺省的) ，可用于修饰属性、方法、类。 只能在本类中、本包中
+ *           （注意: default 并不是访问控制修饰符的关键字，在什么都不加的情况下，就是 default）
+ *
+ * protected : 受保护的，可用于修饰属性、方法。 可以在本类中、本包中、子类中
+ *
+ * public : 公共的，可用于修饰属性、方法、类。 任何地方都可以访问
+ */
+public class ModifyTest {
+    public static void main(String[] args) {
+        Customer cust = new Customer();
+        cust.name = "张三";
+
+        Employee emp = new Employee();
+        //emp.name = "";
+    }
+}
