8ae9c3efd3c5cb68e16416ad8e9370c896fda562
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-26 18:53:46 +0800

    Create Test02.java

---
+++ javadoc/javacommonlyusedclass/oop/src/com/java/oopcomprehensive/review/Test02.java
@@ -0,0 +1,30 @@
+package com.java.oopcomprehensive.review;
+
+public class Test02 {
+    public static void main(String[] args) {
+        Father f = new Son();
+        System.out.println(f.x);
+        //System.out.println("Hello");
+    }
+}
+class Father{
+    int x = 10;
+    public Father(){
+        this.print();
+        x = 20;
+    }
+    public void print(){
+        System.out.println("Father.x = " + x);
+    }
+}
+class Son extends Father{
+    int x = 30;
+    public Son(){
+        //super();
+        this.print();
+        x = 40;
+    }
+    public void print(){
+        System.out.println("Son.x = " + x);
+    }
+}
