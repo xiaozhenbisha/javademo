899f06830e24bdb7afba937e5250a0a4dda297bd
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-26 19:25:38 +0800

    Create MyComparator.java

---
+++ javadoc/javacommonlyusedclass/oop/src/com/java/interfaceandinnerclass/MyComparator.java
@@ -0,0 +1,10 @@
+package com.java.interfaceandinnerclass;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+}
