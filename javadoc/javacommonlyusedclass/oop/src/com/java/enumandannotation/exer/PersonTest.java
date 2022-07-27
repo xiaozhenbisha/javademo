51e78ec17ff38387fd343d9be063e204b49550d6
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-27 13:40:08 +0800

    Create PersonTest.java

---
+++ javadoc/javacommonlyusedclass/oop/src/com/java/enumandannotation/exer/PersonTest.java
@@ -0,0 +1,9 @@
+package com.java.enumandannotation.exer;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.sleep();
+        System.out.println();
+    }
+}
