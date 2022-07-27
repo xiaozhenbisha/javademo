051d9707a13dcbcd2b5ad78cce554180059489a5
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-27 15:09:43 +0800

    Create GCTest.java

---
+++ javadoc/javacommonlyusedclass/oop/src/com/java/stringbuffertest/GCTest.java
@@ -0,0 +1,15 @@
+package com.java.stringbuffertest;
+
+public class GCTest {
+    static{
+        byte[] b = new byte[1024 * 1000 * 1000];
+        System.out.println(b.length);
+    }
+
+
+    public static void main(String[] args) {
+       // GCTest gg = new GCTest();
+       // System.out.println();
+
+    }
+}
