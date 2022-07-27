34f127cc7c45ec1633baa7439573809b5bdf6f4a
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-27 13:40:54 +0800

    Create Rectangle.java

---
+++ javadoc/javacommonlyusedclass/oop/src/com/java/interfaceandinnerclass/exer/Rectangle.java
@@ -0,0 +1,60 @@
+package com.java.interfaceandinnerclass.exer;
+
+public class Rectangle implements Sortable{
+    private double length;
+    private double width;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                '}';
+    }
+
+    //计算矩形面积
+    public double area(){
+        return length * width;
+    }
+
+    @Override
+    public int compare(Sortable s) {
+        if(s instanceof Rectangle){
+            Rectangle r = (Rectangle) s;
+
+            if(this.area() > r.area()){
+                return 1;
+            }else if(this.area() < r.area()){
+                return -1;
+            }else{
+                return 0;
+            }
+        }
+
+        return 666;
+    }
+}
