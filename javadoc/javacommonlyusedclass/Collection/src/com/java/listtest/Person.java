4be9be3e9aabf965e6aa8125fd2eee5286a72270
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-28 09:28:55 +0800

    Create Person.java

---
+++ javadoc/javacommonlyusedclass/Collection/src/com/java/listtest/Person.java
@@ -0,0 +1,52 @@
+package com.java.listtest;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    //重写 Object 类的 hashCode 方法，若对象的内容一样，生成的 hashCode 值也一样
+    public int hashCode(){
+        return name.hashCode() + age * 31;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                name.equals(person.name);
+    }
+}
