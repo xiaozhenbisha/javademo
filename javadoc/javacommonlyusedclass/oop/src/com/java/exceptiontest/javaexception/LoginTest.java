e9437c3df6090014c49d88dad778c76d8b4becc6
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-28 09:29:31 +0800

    Create LoginTest.java

---
+++ javadoc/javacommonlyusedclass/oop/src/com/java/exceptiontest/javaexception/LoginTest.java
@@ -0,0 +1,21 @@
+package com.java.exceptiontest.javaexception;
+
+public class LoginTest {
+    public static void main(String[] args) {
+        try {
+            login("admin", "123456");
+        } catch (UsernamePasswordErrorException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //模拟登陆功能
+    public static void login(String username, String password){
+        if("admin".equals(username) && "123456".equals(password)){
+            System.out.println("登陆成功！");
+        }else{
+            throw new UsernamePasswordErrorException("用户名或密码错误！");
+        }
+    }
+}
