3d9c16282fe4d110b397c1bbd64814dba722b597
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-28 23:05:30 +0800

    Create GenericExer2.java

---
+++ javadoc/javacommonlyusedclass/Collection/src/com/java/collectiontest/exer/GenericExer2.java
@@ -0,0 +1,43 @@
+package com.java.collectiontest.exer;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.TreeSet;
+
+public class GenericExer2 {
+    @Test
+    public void test1(){
+        DAO<Student> dao = new DAO<>();
+
+        dao.save("AA", new Student("张三", 99));
+
+        Student stu = dao.get("AA");
+        System.out.println(stu);
+
+        List<Student> stus = dao.list();
+
+        Iterator<Student> it = stus.iterator();
+
+        while(it.hasNext()){
+            Student stu2 = it.next();
+            System.out.println(stu2);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        TreeSet<Student> ts = new TreeSet<>();
+        ts.add(new Student("张三", 99));
+        ts.add(new Student("李四", 77));
+        ts.add(new Student("王五", 88));
+        ts.add(new Student("赵六", 55));
+        ts.add(new Student("田七", 66));
+
+        for(Student stu : ts){
+            System.out.println(stu);
+        }
+    }
+
+}
