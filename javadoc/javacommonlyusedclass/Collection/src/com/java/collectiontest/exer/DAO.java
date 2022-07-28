e1ff93309d5d2086a7a54c361f842a5641510cd2
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-28 23:05:23 +0800

    Update DAO.java

--- javadoc/javacommonlyusedclass/Collection/src/com/java/collectiontest/exer/DAO.java
+++ javadoc/javacommonlyusedclass/Collection/src/com/java/collectiontest/exer/DAO.java
@@ -1,4 +1,34 @@
 package com.java.collectiontest.exer;
 
-public class DAO {
+import java.util.*;
+
+public class DAO<T> {
+
+    private Map<String, T> map = new HashMap<>();
+
+    public void save(String id, T entity){
+        map.put(id, entity);
+    }
+
+    public T get(String id){
+        return map.get(id);
+    }
+
+    public void update(String id, T entity){
+        map.put(id, entity);
+    }
+
+    public List<T> list(){
+        Collection<T> coll = map.values();
+
+        List<T> list = new ArrayList<>();
+
+        list.addAll(coll);
+
+        return list;
+    }
+
+    public void delete(String id){
+        map.remove(id);
+    }
 }
