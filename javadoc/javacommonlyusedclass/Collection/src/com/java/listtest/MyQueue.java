d70546c0dc4c1ec55a91cec52156f92a1784f24b
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-28 09:28:52 +0800

    Create MyQueue.java

---
+++ javadoc/javacommonlyusedclass/Collection/src/com/java/listtest/MyQueue.java
@@ -0,0 +1,26 @@
+package com.java.listtest;
+
+import java.util.LinkedList;
+
+/**
+ * 堆栈：先进后出
+ * 队列：先进先出
+ */
+public class MyQueue {
+    private LinkedList ll = new LinkedList();
+
+    //添加
+    public void add(Object obj){
+        ll.addFirst(obj);
+    }
+
+    //获取
+    public Object get(){
+        return ll.removeLast();
+    }
+
+    //判断
+    public boolean isNull(){
+        return ll.isEmpty();
+    }
+}
