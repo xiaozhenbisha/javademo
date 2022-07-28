295ecc5a4a42103c576e834861641277bb591071
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-28 09:31:26 +0800

    Create Programmer.java

---
+++ javadoc/javacommonlyusedclass/TeamSchedule/src/com/java/teamschedule/domain/Programmer.java
@@ -0,0 +1,58 @@
+package com.java.teamschedule.domain;
+
+/**
+* 程序员类
+* @author Hefei Li
+* 2020/6/12 15:22
+*/
+public class Programmer extends Employee{
+
+    private int memberId; //成员 ID
+    private Status status = Status.FREE; //默认状态为:空闲
+    private Equipment equipment;
+
+    public Programmer() {
+    }
+
+    public Programmer(int id, String name, int age, double salary, Equipment equipment) {
+        super(id, name, age, salary);
+        this.equipment = equipment;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Equipment getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(Equipment equipment) {
+        this.equipment = equipment;
+    }
+
+    public String getMemberForTeam(){
+        return getMemberId() + "/" + getDetails();
+    }
+
+    public String getDetailsForTeam(){
+        return getMemberForTeam() + "\t程序员\t";
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "\t程序员\t" + getStatus() + "\t\t\t\t\t" + getEquipment();
+    }
+}
