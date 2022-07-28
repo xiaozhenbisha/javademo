eec6592db6cd2f4cddbe7b86e72ca53b0c950c92
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-28 09:29:33 +0800

    Create MyException.java

---
+++ javadoc/javacommonlyusedclass/oop/src/com/java/exceptiontest/javaexception/MyException.java
@@ -0,0 +1,12 @@
+package com.java.exceptiontest.javaexception;
+
+public class MyException extends RuntimeException{
+    public MyException(){
+
+    }
+
+    public MyException(String message){
+        super(message);
+    }
+
+}
