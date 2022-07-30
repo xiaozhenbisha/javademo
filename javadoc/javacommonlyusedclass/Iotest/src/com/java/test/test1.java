177224a654ff324fe7c39c1fa20115c0a0afeb08
xiaozhenbisha <49591948+xiaozhenbisha@users.noreply.github.com>
2022-07-30 23:35:14 +0800

    Create test1.java

---
+++ javadoc/javacommonlyusedclass/Iotest/src/com/java/test/test1.java
@@ -0,0 +1,48 @@
+package com.java.test;
+
+import org.junit.Test;
+
+import java.io.*;
+
+/**
+ * 将一个文件从d:iotest/java.txt复制到d:testIO/javaEE.txt
+ */
+public class test1 {
+    @Test
+    public void test1(){
+        //1. 流对象的创建
+        InputStream in=null;
+        OutputStream out=null;
+        try {
+            in=new FileInputStream("/Users/wangzhennan/Desktop/javademo/javadoc/testio/java.txt");
+            out=new FileOutputStream("/Users/wangzhennan/Desktop/javademo/javadoc/testio/javaEE1.txt");
+            //2. 读写操作
+            //2.1
+            /*int i=0;
+            while((i=in.read())!=-1){
+                out.write(i);
+            }*/
+            //2.2
+            byte[] b=new byte[1024];
+            int len=0;
+            while((len=in.read(b))!=-1){
+                out.write(b,0,len);//会有重复数据
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            //3. 流资源的关闭
+            try {
+                if(in!=null)
+                    in.close();
+                if(out!=null)
+                    out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+    }
+}
